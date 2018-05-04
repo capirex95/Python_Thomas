d282a26859108f7e3d2c4dcd55a71060e0b54379
root <root@python.course.lab>
2018-05-04 17:01:55 +0200

    ajout script prof + debuggage

---
+++ snifferv2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+
+from snifferv1 import snifferv1
+
+
+def main():
+    s = socket.socket(
+            socket.AF_PACKET,
+            socket.SOCK_RAW,
+            socket.htons(3)
+    )
+    s.bind(('ens33', 3))
+
+    counter = 0
+    while True:
+        counter += 1
+        try:
+            message = s.recv(1024)
+            ethernet_frame = snifferv1(message)
+            print('{:0>5d} - DSTADD: {}'.format(counter, ethernet_frame.destination_address))
+            print('{:0>5d} - SRCADD: {}'.format(counter, ethernet_frame.source_address))
+            print('{:0>5d} - ETHTYP: {}'.format(counter, ethernet_frame.ethertype))
+        except KeyboardInterrupt:
+            print('Sniffer stopped')
+            break
+
+
+if __name__ == '__main__':
+ main()
